859fd65a99958596876aaa75385ec69a9f80dad6
Jacob Schwartz <jaschwa@umich.edu>
2021-07-21 16:49:24 -0400

    log sls deploy to file and save api url to file

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,7 @@
 # Using this script because github action passess the arguments as strings
 # instead of array
 echo ">>> Running sls $*"
-/app/node_modules/.bin/sls $@
+/app/node_modules/.bin/sls $@ | tee deploy.log
+egrep "ANY -.*/$" deploy.log | awk '{gsub("  ANY - ", "");print}' > .api_url
+echo "saved api url to file .api_url"
 echo ">>> Completed"
