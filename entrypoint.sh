500015a18a9322657b045ed63096a7821306bec1
Jacob Schwartz <jaschwa@umich.edu>
2021-07-23 14:21:44 -0400

    oopsy

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2068
 # Using this script because github action passess the arguments as strings
 # instead of array
-echo ">>> Running sls $*"'
+echo ">>> Running sls $*"
 cd api
 /app/node_modules/.bin/sls $@ | tee deploy.log
 egrep "ANY -.*/$" deploy.log | awk '{gsub("  ANY - ", "");print}' > .api_url
