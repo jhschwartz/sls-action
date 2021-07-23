026d30de2841b4e29e65524cdd2fef0908c0eb60
Jacob Schwartz <jaschwa@umich.edu>
2021-07-23 10:52:18 -0400

    work in api dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 # shellcheck disable=SC2068
 # Using this script because github action passess the arguments as strings
 # instead of array
-echo ">>> Running sls $*"
+echo ">>> Running sls $*"'
+cd api
 /app/node_modules/.bin/sls $@ | tee deploy.log
 egrep "ANY -.*/$" deploy.log | awk '{gsub("  ANY - ", "");print}' > .api_url
-cp .api_url api/ # in case we are working in subdir
 echo "saved api url to file .api_url"
 echo ">>> Completed"
